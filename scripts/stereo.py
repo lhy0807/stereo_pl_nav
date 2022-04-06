fbbcc59fb2035bff7a4ae54b871fae086dcc6b49
Hongyu Li <lihongyu0807@icloud.com>
2022-04-06 00:17:12 -0400

    [add] add rgb point cloud

--- scripts/stereo.py
+++ scripts/stereo.py
@@ -15,7 +15,7 @@ from mobilestereonet.models.MSNet3D import MSNet3D
 import logging
 import coloredlogs
 import time
-from collections import OrderedDict
+import open3d as o3d
 
 logger = logging.getLogger(__name__)
 coloredlogs.install(level='DEBUG', logger=logger)
@@ -41,7 +41,7 @@ class Stereo():
         model = nn.DataParallel(model)
         ckpt_path = os.path.join(CURR_DIR, "models/MSNet2D_SF_DS_KITTI2015.ckpt")
         logger.info("model {} loaded".format(ckpt_path))
-        state_dict = torch.load(ckpt_path, map_location="cpu")
+        state_dict = torch.load(ckpt_path, map_location="cuda")
         model.load_state_dict(state_dict['model'])
         self.model = model
 
@@ -49,6 +49,9 @@ class Stereo():
         if self.model is None or self.right_camera_info is None:
             return
         processed = get_transform()
+
+        left_depth_rgb = self.left_rect[:, :, :3]
+        depth_rgb = np.transpose(left_depth_rgb, (2, 0, 1))
 
         sample_left = processed(self.left_rect).numpy()
         sample_right = processed(self.right_rect).numpy()
@@ -68,6 +71,24 @@ class Stereo():
         b_x = self.right_camera_info.P[3] / (-f_u)  # relative
         b_y = self.right_camera_info.P[7] / (-f_v)
 
+        def project_image_to_rect(uv_depth):
+            ''' Input: nx3 first two channels are uv, 3rd channel
+                    is depth in rect camera coord.
+                Output: nx3 points in rect camera coord.
+            '''
+            n = uv_depth.shape[0]
+            x = ((uv_depth[:, 0] - c_u) * uv_depth[:, 2]) / f_u + b_x
+            y = ((uv_depth[:, 1] - c_v) * uv_depth[:, 2]) / f_v + b_y
+            pts_3d_rect = np.zeros((n, 3))
+            pts_3d_rect[:, 0] = x
+            pts_3d_rect[:, 1] = y
+            pts_3d_rect[:, 2] = uv_depth[:, 2]
+
+            return pts_3d_rect
+
+        def project_image_to_velo(uv_depth):
+            pts_3d_rect = project_image_to_rect(uv_depth)
+            return pts_3d_rect
 
         with torch.no_grad():
             disp_est_tn = self.model(sample_left, sample_right)[0]
@@ -82,6 +103,22 @@ class Stereo():
             depth = f_u * baseline / (disp_est + 1. - mask)
             self.depth_image_pub.publish(
                 self.bridge.cv2_to_imgmsg(depth))
+
+            # display point cloud
+            rows, cols = depth.shape
+            c, r = np.meshgrid(np.arange(cols), np.arange(rows))
+            points = np.stack([c, r, depth])
+            points = points.reshape((3, -1))
+            points = points.T
+            points = points[mask.reshape(-1)]
+            cloud = project_image_to_velo(points)
+            points_rgb = depth_rgb.reshape((3, -1)).T
+            points_rgb = points_rgb.astype(float)
+            points_rgb /= 255.
+
+            rgbd_pcd = o3d.geometry.PointCloud()
+            rgbd_pcd.points = o3d.utility.Vector3dVector(cloud)
+            rgbd_pcd.colors = o3d.utility.Vector3dVector(points_rgb)
             
     def __init__(self) -> None:
         self.bridge = CvBridge()
