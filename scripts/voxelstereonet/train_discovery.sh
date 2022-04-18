3f3c2cbf378f6ef108a2d3f28a054c100accc76a
Hongyu Li <lihongyu0807@icloud.com>
2022-04-18 12:06:34 -0400

    [change] add epochs

--- scripts/voxelstereonet/train_discovery.sh
+++ scripts/voxelstereonet/train_discovery.sh
@@ -2,5 +2,5 @@
 python train.py --dataset voxel --datapath /work/riverlab/hongyu/dataset/SceneFlow \
  --trainlist /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/filenames/sceneflow_train.txt \
  --testlist /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/filenames/sceneflow_test.txt \
- --epochs 20 --lrepochs "10,12,14,16:2" --logdir /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/logs \
- --batch_size 8
+ --epochs 50 --lrepochs "10,12,14,16:2" --logdir /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/logs \
+ --batch_size 8 --test_batch_size 8
