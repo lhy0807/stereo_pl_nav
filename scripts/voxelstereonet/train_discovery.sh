60b80b6fbb3fa3b94d6835417ea831ee6332dab0
Hongyu Li <lihongyu0807@icloud.com>
2022-04-17 20:37:51 -0400

    add training script for Discovery cluster

---
+++ scripts/voxelstereonet/train_discovery.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+python train.py --dataset voxel --datapath /work/riverlab/hongyu/dataset/SceneFlow \
+ --trainlist /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/filenames/sceneflow_train.txt \
+ --testlist /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/filenames/sceneflow_test.txt \
+ --epochs 20 --lrepochs "10,12,14,16:2" --logdir /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/logs \
+ --batch_size 8
