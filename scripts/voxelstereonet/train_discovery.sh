4971c4cea4a65ef8f7e5e0829e6c739481b44ba6
Hongyu Li <lihongyu0807@icloud.com>
2022-04-21 22:52:58 -0400

    [change]cluster training script

--- scripts/voxelstereonet/train_discovery.sh
+++ scripts/voxelstereonet/train_discovery.sh
@@ -3,4 +3,4 @@ python train.py --dataset voxel --datapath /work/riverlab/hongyu/dataset/SceneFl
  --trainlist /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/filenames/sceneflow_train.txt \
  --testlist /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/filenames/sceneflow_test.txt \
  --epochs 30 --lrepochs "10,15,20,25:2" --logdir /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/logs \
- --batch_size 8 --test_batch_size 8
+ --batch_size 8 --test_batch_size 8 --summary_freq 32 --loader_workers 8
