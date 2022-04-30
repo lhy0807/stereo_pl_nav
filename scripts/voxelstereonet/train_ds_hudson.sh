f6590bd1e94bf1c843762c9aa4d3048fbc520e4e
Hongyu Li <lihongyu0807@icloud.com>
2022-04-30 17:54:47 -0400

    [change]hudson script

--- scripts/voxelstereonet/train_ds_hudson.sh
+++ scripts/voxelstereonet/train_ds_hudson.sh
@@ -3,4 +3,4 @@ python train.py --dataset voxelds --datapath /DS \
  --trainlist /scratch/yw3076/lhy/stereo_pl_nav/scripts/voxelstereonet/filenames/DS_train.txt \
  --testlist /scratch/yw3076/lhy/stereo_pl_nav/scripts/voxelstereonet/filenames/DS_test.txt \
  --epochs 20 --lrepochs "10,16:2" --logdir /scratch/yw3076/lhy/stereo_pl_nav/scripts/voxelstereonet/logs \
- --batch_size 32 --test_batch_size 16 --summary_freq 32 --loader_workers 8 --lr 1e-3
+ --batch_size 36 --test_batch_size 16 --summary_freq 32 --loader_workers 8 --lr 1e-3 --log_folder_suffix odds
