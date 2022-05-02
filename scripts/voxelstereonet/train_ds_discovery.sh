39883d6918ac8c7666a05ef2280bcaf895b71d94
Hongyu Li <lihongyu0807@icloud.com>
2022-05-01 22:44:53 -0700

    [change]discovery training script

--- scripts/voxelstereonet/train_ds_discovery.sh
+++ scripts/voxelstereonet/train_ds_discovery.sh
@@ -2,5 +2,5 @@
 python train.py --dataset voxelds --datapath /work/riverlab/hongyu/dataset/DS \
  --trainlist /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/filenames/DS_train.txt \
  --testlist /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/filenames/DS_test.txt \
- --epochs 30 --lrepochs "10,15,20,25:2" --logdir /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/logs \
- --batch_size 8 --test_batch_size 8 --summary_freq 50 --loader_workers 8
+ --epochs 10 --lrepochs "10,16:2" --logdir /work/riverlab/hongyu/stereo_pl_nav/scripts/voxelstereonet/logs \
+ --batch_size 16 --test_batch_size 16 --summary_freq 50 --loader_workers 8
