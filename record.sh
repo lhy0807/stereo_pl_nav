5fb0c0940ab8206529f76efdc4e54f7aa6464b1d
vanessahadlock <hadlock.v@northeastern.edu>
2022-05-25 01:24:42 -0400

    [update]calibration

--- record.sh
+++ record.sh
@@ -1,4 +1,4 @@
-rosbag record /tf /tf_static /jackal_velocity_controller/odom \
- /zed2/left/camera_info /zed2/left/image_raw /zed2/left/image_rect_color \
- /zed2/right/camera_info /zed2/right/image_color /zed2/right/image_rect_color \
+rosbag record /tf /tf_static /imu/data /imu/data_raw /imu/mag \
+ /zed2/left/camera_info  /zed2/left/image_rect_color \
+ /zed2/right/camera_info /zed2/right/image_rect_color \
  /rslidar_points
